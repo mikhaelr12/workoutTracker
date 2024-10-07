7eaaf44a028123a4bbaf48ac9c686964d14175c2
mikhaelr12 <rusumiihai@gmail.com>
2024-10-07 17:01:41 +0300

    initial project

---
+++ src/test/java/md/orange/workouttrackerapi/WorkoutTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package md.orange.workouttrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutTrackerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
